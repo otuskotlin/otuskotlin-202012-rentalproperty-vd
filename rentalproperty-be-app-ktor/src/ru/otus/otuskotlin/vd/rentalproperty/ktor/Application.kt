d48cf5feb0e35e124ce5f9a6463f01050613b61d
zundarik <zundarik@gmail.com>
2021-03-21 01:44:20 +1100

    ktor move classes to controller package ->stdlib-jdk8

--- rentalproperty-be-app-ktor/src/ru/otus/otuskotlin/vd/rentalproperty/ktor/Application.kt
+++ rentalproperty-be-app-ktor/src/ru/otus/otuskotlin/vd/rentalproperty/ktor/Application.kt
@@ -4,25 +4,17 @@ import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.http.content.*
-import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
-import ru.otus.otuskotlin.vd.rentalproperty.ktor.service.AdvertRentHouseService
-import ru.otus.otuskotlin.vd.rentalproperty.ktor.service.HouseService
-import ru.otus.otuskotlin.vd.rentalproperty.transport.kmp.models.advert.house.*
-import ru.otus.otuskotlin.vd.rentalproperty.transport.kmp.models.common.Message
-import ru.otus.otuskotlin.vd.rentalproperty.transport.kmp.models.common.ResponseStatusDto
-import ru.otus.otuskotlin.vd.rentalproperty.transport.kmp.models.realty.house.*
+import ru.otus.otuskotlin.vd.rentalproperty.ktor.controller.advertHouseRoute
+import ru.otus.otuskotlin.vd.rentalproperty.ktor.controller.houseRoute
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-
-  val houseService = HouseService()
-  val advertHouseService = AdvertRentHouseService()
 
   install(CORS) {
     method(HttpMethod.Options)
@@ -52,131 +44,8 @@ fun Application.module(testing: Boolean = false) {
       resources("static")
     }
 
-    route("/realty/houses") {
-      post("/get") {
-        try {
-          val query = call.receive<Message>() as RequestHouseRead
-          call.respond(houseService.get(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseHouseRead(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-      post("/create") {
-        try {
-          val query = call.receive<Message>() as RequestHouseCreate
-          call.respond(houseService.create(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseHouseCreate(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-      post("/update") {
-        try {
-          val query = call.receive<Message>() as RequestHouseUpdate
-          call.respond(houseService.update(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseHouseUpdate(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-      post("/delete") {
-        try {
-          val query = call.receive<Message>() as RequestHouseDelete
-          call.respond(houseService.delete(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseHouseDelete(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-      post("/filter") {
-        try {
-          val query = call.receive<Message>() as RequestHouseList
-          call.respond(houseService.filter(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseHouseList(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-    }
-
-    route("/adverts/houses") {
-      post("/get") {
-        try {
-          val query = call.receive<Message>() as RequestAdvertRentHouseRead
-          call.respond(advertHouseService.get(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseAdvertRentHouseRead(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-      post("/create") {
-        try {
-          val query = call.receive<Message>() as RequestAdvertRentHouseCreate
-          call.respond(advertHouseService.create(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseAdvertRentHouseCreate(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-      post("/update") {
-        try {
-          val query = call.receive<Message>() as RequestAdvertRentHouseUpdate
-          call.respond(advertHouseService.update(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseAdvertRentHouseUpdate(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-      post("/delete") {
-        try {
-          val query = call.receive<Message>() as RequestAdvertRentHouseDelete
-          call.respond(advertHouseService.delete(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseAdvertRentHouseDelete(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-      post("/filter") {
-        try {
-          val query = call.receive<Message>() as RequestAdvertRentHouseList
-          call.respond(advertHouseService.filter(query))
-        } catch (e: Throwable) {
-          call.respond(
-            ResponseAdvertRentHouseList(
-              status = ResponseStatusDto.BAD_REQUEST
-            )
-          )
-        }
-      }
-    }
+    houseRoute()
+    advertHouseRoute()
   }
 }
 
--- rentalproperty-mp-transport-mp/build.gradle.kts
+++ rentalproperty-mp-transport-mp/build.gradle.kts
@@ -54,7 +54,7 @@ kotlin {
     }
     val jvmMain by getting {
       dependencies {
-        implementation(kotlin("stdlib"))
+        implementation(kotlin("stdlib-jdk8"))
       }
     }
     val jvmTest by getting {
